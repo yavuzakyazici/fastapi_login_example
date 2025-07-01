d1437918083c71708640cce90688ff5c80adbec2
yavuzakyazici <yakyazici@gmail.com>
2025-07-01 15:23:32 +0300

    corrected bcrypt version

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 from fastapi import FastAPI
-from fastapi.security import  OAuth2PasswordBearer
-from passlib.context import CryptContext
 from login import user_login_router
 from db import Base, engine
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_user_scheme = OAuth2PasswordBearer(
-    tokenUrl="/user/login/token",
-    scheme_name="oauth2_user_scheme",
-    scopes={"user": "allowed methods for authenticated users"},
-)
 
 app = FastAPI()
 app.include_router(user_login_router)
